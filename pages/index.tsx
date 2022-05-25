0defe3c26f4eb6713f5b67e1f50478915866771d
Adam Luzsi <luzsiadam@gmail.com>
2022-05-25 15:54:48 +0200

    + Added index TSX

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+import { ClientsTableContainerWithAsyncClass } from '../src/clients/ClientsTableContainer'
+import { ErrorBoundary } from '../src/common/ErrorBoundary'
+import { useAuthContext } from '../src/auth/AuthContext'
+import { AuthGuard } from '../src/auth/AuthGuard'
+
+const Home: NextPage = () => {
+  const { logout } = useAuthContext()
+
+  return (
+    <AuthGuard>
+      <ErrorBoundary>
+        <div className={styles.container}>
+          {/* <h2>We'll be back at 17:14 on this PC's clock</h2> */}
+          <button onClick={logout}>Log out</button>
+          <ErrorBoundary scope='clients' errorCompoennt={(<div>Ups something went wrong</div>)}>
+              <ClientsTableContainerWithAsyncClass />
+          </ErrorBoundary>
+        </div>
+      </ErrorBoundary>
+    </AuthGuard>
+  )
+}
+
+export default Home
