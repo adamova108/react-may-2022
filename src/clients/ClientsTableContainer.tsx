ea7982d7d82b850b691232d78bb6c159ce8c4650
Adam Luzsi <luzsiadam@gmail.com>
2022-06-03 18:00:54 +0200

    + Added ClientsTableContainer

---
+++ src/clients/ClientsTableContainer.tsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import { fetchClients } from "../api/base"
+import { useAsync, UseAsyncClass } from "../utils/useAsync";
+import { ClientsTable } from "./ClientsTable";
+
+export const ClientsTableContainer = () => {
+
+    const {status, value, error, execute } = useAsync(fetchClients)
+
+    useEffect(() => {
+        execute(undefined)
+    }, [])
+
+    if ( status === 'idle' || status === 'pending' ) {
+        // loading idicator
+        return <div>Loading</div>;
+    }
+
+    if ( status === 'error' ) {
+        // error indicator
+        console.log(error)
+        return <div>Error</div>
+    }
+
+    if ( !value ) {
+        return null
+    }
+    
+    return (
+        <ClientsTable {...value.data} />
+    )
+}
+
+
+export const ClientsTableContainerWithAsyncClass = () => {
+    return (
+        <UseAsyncClass
+            asyncFunction={fetchClients}
+            immediate
+        >
+            {
+                ({status, error, value}) => {
+                    if ( status === 'idle' || status === 'pending' ) {
+                        // loading idicator
+                        return <div>Loading</div>;
+                    }
+                
+                    if ( status === 'error' ) {
+                        // error indicator
+                        console.log(error)
+                        return <div>Error</div>
+                    }
+                
+                    if ( !value ) {
+                        return null
+                    }
+                    return (
+                        <ClientsTable {...value.data} />
+                    )
+                }
+            }
+        </UseAsyncClass>    
+    )
+}
