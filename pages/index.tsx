b98108363e04e55ccd976d7fdf45ca9ff9b9bafb
Adam Luzsi <luzsiadam@gmail.com>
2022-06-07 08:54:37 +0200

    + Updated index TSX

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-import { ClientsTableContainerWithAsyncClass } from '../src/clients/ClientsTableContainer'
+import { ClientsTableContainer } from '../src/clients/ClientsTableContainer'
 import { ErrorBoundary } from '../src/common/ErrorBoundary'
 import { useAuthContext } from '../src/auth/AuthContext'
 import { AuthGuard } from '../src/auth/AuthGuard'
 
 const Home: NextPage = () => {
-  const { logout } = useAuthContext()
+    const { logout } = useAuthContext()
 
-  return (
-    <AuthGuard>
-      <ErrorBoundary>
-        <div className={styles.container}>
-          {/* <h2>We'll be back at 17:14 on this PC's clock</h2> */}
-          <button onClick={logout}>Log out</button>
-          <ErrorBoundary scope='clients' errorCompoennt={(<div>Ups something went wrong</div>)}>
-              <ClientsTableContainerWithAsyncClass />
-          </ErrorBoundary>
-        </div>
-      </ErrorBoundary>
-    </AuthGuard>
-  )
+    return (
+        <AuthGuard>
+            {/* <h2>Be back at 16:57 on this PC's clock</h2> */}
+            <ErrorBoundary>
+                <div className={styles.container}>
+                    <button onClick={logout}>Log out</button>
+                    <ErrorBoundary scope='clients' errorCompoennt={(<div>Ups something went wrong</div>)}>
+                        <ClientsTableContainer />
+                    </ErrorBoundary>
+                </div>
+            </ErrorBoundary>
+        </AuthGuard>
+    )
 }
 
 export default Home
