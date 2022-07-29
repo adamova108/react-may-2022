33c23ff543f63d9bd1eb4acbc886bf8e7c82709a
Adam Luzsi <luzsiadam@gmail.com>
2022-07-29 19:02:23 +0200

    + Added ClientsStore TSX

---
+++ src/clients/ClientsStore.tsx
@@ -0,0 +1,68 @@
+import { AxiosError } from "axios"
+import produce from "immer"
+import create from "zustand"
+import { ClientsRestApiResponse, ClientsDTO, fetchClients, FetchClientsParams, fetchGraphQLClients } from "../api/base"
+
+type ClientStoreType = {
+    clientsList: {
+        clients: null | ClientsDTO[]
+        total: number | null
+        fetchStatus: "error" | "idle" | "pending" | "success"
+        error: null | string
+    }
+    addClient: Function
+    fetchClisntsList: (params: FetchClientsParams) => Promise<ClientsRestApiResponse>
+}
+
+export const useClientsStore = create<ClientStoreType>(set => ({
+    clientsList: {
+        clients: [],
+        total: null,
+        fetchStatus: 'idle',
+        error: null,
+    },
+
+    addClient: () => {},
+
+    fetchClisntsList: async (params: FetchClientsParams) => {
+    
+        set(produce((state: ClientStoreType) => {
+            const { clientsList } = state;
+            clientsList.fetchStatus = 'pending'
+            clientsList.clients = null
+            clientsList.total = null
+            clientsList.error = null
+        }))
+
+        try {
+            const clientsResponse = await fetchGraphQLClients(params)
+            set(produce((state: ClientStoreType) => {
+                const { clientsList } = state;
+                clientsList.fetchStatus = 'success'
+                clientsList.clients = clientsResponse.clients
+                clientsList.total = clientsResponse.total
+            }))
+
+            return clientsResponse
+        } catch (error) {
+            if ( error instanceof AxiosError ) {
+                const errorString = error.response?.data;
+                set(state => {
+                    return {
+                        ...state,
+                        error: errorString
+                    }
+                })
+                return Promise.reject(errorString)
+            } else {
+                set(state => {
+                    return {
+                        ...state,
+                        error: "Unknown Error"
+                    }
+                })
+                return Promise.reject(`Unknown Error: ${error}`)
+            }
+        }
+    },
+}))
