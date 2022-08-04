435c8431184e494f0dac63d3c469b1f5975bab7f
Adam Luzsi <luzsiadam@gmail.com>
2022-08-04 15:02:31 +0200

    + Updated Next Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+    reactStrictMode: false,
+    compiler: {
+        reactRemoveProperties: process.env.NODE_ENV === 'production'
+    },
+    eslint: {
+        dirs: ['pages', 'src']
+    },
 }
 
 module.exports = nextConfig
