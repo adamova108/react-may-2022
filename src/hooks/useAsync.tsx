c8e7eb0c3b242883d1156da51b95aca9f67e10b7
Adam Luzsi <luzsiadam@gmail.com>
2022-08-14 17:27:19 +0200

    + Added useAsync

---
+++ src/hooks/useAsync.tsx
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react"
+
+export const enum AsyncState {
+    INITIAL,
+    PENDING,
+    SUCCESS,
+    ERROR
+}
+
+export const useAsync = <T, P, E = string>(asyncOperation: (params: P) => Promise<T>) => {
+
+    const [status, setStatus] = useState<AsyncState>(AsyncState.INITIAL)
+    const [error, setError] = useState<E | null>(null)
+    const [data, setData] = useState<T | null>(null)
+
+    const execute = useCallback(async (params: P) => {
+        setStatus(AsyncState.PENDING)
+        try {
+            const response = await asyncOperation(params)
+            setStatus(AsyncState.SUCCESS)
+            setData(response)
+
+            // errors handled here should be simple text messages
+            // anything more complicated should be handled in other layers
+        } catch (err: any) {
+            setError(err.toString())
+            setStatus(AsyncState.ERROR)
+        }
+    }, [asyncOperation])
+
+    return {
+        status,
+        data,
+        error,
+        execute
+    }
+}
