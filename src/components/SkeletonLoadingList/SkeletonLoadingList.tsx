e1adfd1cc102fecb801e610b26e736c35dbc71dd
Adam Luzsi <luzsiadam@gmail.com>
2022-08-17 15:14:35 +0200

    + Added logging to SkeletonLoadingList

--- src/components/SkeletonLoadingList/SkeletonLoadingList.tsx
+++ src/components/SkeletonLoadingList/SkeletonLoadingList.tsx
@@ -1,6 +1,9 @@
 import { Box, Skeleton } from "@mui/material"
+import { useAuthConext } from "../../user/AuthContext"
 
 export const SkeletonLoadingList = () => {
+    const authContext = useAuthConext()
+    console.log(authContext)
     return (
         <Box sx={{ width: 300 }}>
             <Skeleton animation="wave" />
