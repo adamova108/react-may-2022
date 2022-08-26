fe03aad695aa691b4b9ff13547e537a41796eb04
Adam Luzsi <luzsiadam@gmail.com>
2022-08-26 19:35:25 +0200

    + Added AuthContext

--- src/user/LoginFormContainer.tsx
+++ src/user/LoginFormContainer.tsx
@@ -1,16 +1,21 @@
 import { AuthApi } from "../api/auth"
 import { AsyncState, useAsync } from "../hooks/useAsync"
+import { useAuthConext } from "./AuthContext"
 import { LoginForm } from "./LoginForm"
 
 export const LoginFormContainer = () => {
     const { status, execute } = useAsync(AuthApi.login)
-
+    const authContext = useAuthConext()
     return (
         <LoginForm 
             submitDisabled={status === AsyncState.PENDING}
             onLoginRquest={async (values) => {
                 // sets some flag to loading
-                await execute(values)
+                const result = await execute(values)
+                console.log(result)
+                // JSON check, see if result is valid
+                // result contains token???
+                authContext.login(result!.token)
             }} />
     )
 }
