656f5bb206001762511a1dad63d2165b82ca498a
Adam Luzsi <luzsiadam@gmail.com>
2023-02-20 17:12:31 +0100

    + Added usersRepo

---
+++ src/repositories/usersRepository.ts
@@ -0,0 +1,40 @@
+import { jsonDir } from '../config';
+import { BaseRepository } from './baseRepository';
+import { CompanyDetails } from './company';
+
+type UserData = {
+    id: string
+    email: string
+    password: string
+    companyDetails?: CompanyDetails
+};
+
+export class UsersRepository extends BaseRepository<UserData> {
+    private static _instance: UsersRepository;
+    
+    static async getInstance () {
+        if (!UsersRepository._instance) {
+            UsersRepository._instance = new UsersRepository();
+            await UsersRepository._instance.init(jsonDir);
+        }
+        return UsersRepository._instance;
+    }
+    
+    constructor () {
+        super("users.json");
+    }
+
+
+    async getByEmail (email: string) {
+        return this.inMemoryData.find((item) => item.email === email)
+    }
+
+    async add(user: Omit<UserData, "id">) {
+        const { email } = user;
+        const existingUser = await this.getByEmail(email)
+        if ( existingUser ) {
+            throw new Error(`email already used by another account`);
+        }
+        return super.add(user);
+    }
+}
